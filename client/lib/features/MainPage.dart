091a5daa318cae73429445b10c204b638b54d8ac
Razafimanantsoa Johnson <johnsonrazafimanantsoa@gmail.com>
2025-07-26 01:03:13 +0400

    first commit

---
+++ client/lib/features/MainPage.dart
@@ -0,0 +1,66 @@
+import "package:flutter/material.dart";
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        child:  Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:[
+                    Container(
+                      width: 48.0,
+                      height: 48.0,
+                      child: CircleAvatar(
+                          child: Icon(Icons.man)
+                      ),
+                    ),
+                    Container(
+                      width: 48,
+                      height: 48,
+                      child: FloatingActionButton(onPressed: (){},
+                          child: Icon(Icons.menu)
+                      ) ,
+                    ),
+                  ]
+              ),
+              SizedBox(
+                  height: 300
+              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 24.0,
+                  children: [
+                    Container(
+                        width: 320,
+                        child: FloatingActionButton.extended(
+
+                          onPressed: (){
+                            print("we want to scan a bill");
+                          },
+                          label: Text("Scan a bill"),
+                          icon: Image.asset("assets/icons/document_scanner.png"),
+                        )
+                    ),
+                    Container(
+                      width: 320,
+                      child: FloatingActionButton.extended(
+                        onPressed: (){
+                          print("we want to scan a qr");
+                        },
+                        label: Text("Scan a Qr"),
+                        icon: Image.asset("assets/icons/qr_code_scanner.png"),
+                      ),
+                    ),
+                  ]
+              )
+            ]
+        )
+    );
+  }
+}
